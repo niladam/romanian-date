a8129b568a3fd272a14cd6f2351089855761f8ca
Madalin Tache <madalin@madalin.eu>
2019-04-18 17:21:52 +0300

    Remove no longer use nullable function

--- src/RomanianDate.php
+++ src/RomanianDate.php
@@ -2,9 +2,7 @@
 
 namespace Madalin\RomanianDateTime;
 
-use Carbon\Carbon;
 use Laravel\Nova\Fields\Date;
-use Laravel\Nova\Http\Requests\NovaRequest;
 
 class RomanianDate extends Date
 {
@@ -18,15 +16,15 @@ class RomanianDate extends Date
     /**
      * Create a new field.
      *
-     * @param  string  $name
-     * @param  string|null  $attribute
+     * @param  string      $name
+     * @param  string|null $attribute
      * @param  mixed|null  $resolveCallback
      * @return void
      */
     public function __construct($name, $attribute = null, $resolveCallback = null)
     {
         parent::__construct($name, $attribute, $resolveCallback ?? function ($value) {
-            if (! $value instanceof DateTimeInterface) {
+            if ( ! $value instanceof DateTimeInterface) {
                 throw new Exception("Date field must cast to 'date' in Eloquent model.");
             }
 
@@ -37,21 +35,11 @@ class RomanianDate extends Date
     /**
      * Set the date format (Moment.js) that should be used to display the date.
      *
-     * @param  string  $format
+     * @param  string $format
      * @return $this
      */
     public function format($format)
     {
         return $this->withMeta(['format' => $format]);
-    }
-
-    /**
-     * Indicate that the date field is nullable.
-     *
-     * @return $this
-     */
-    public function nullable()
-    {
-        return $this->withMeta(['nullable' => true]);
     }
 }
